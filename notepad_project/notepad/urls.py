39eb1e0657667cb042e5811fbde11cb5ed288dc4
Sivasai Yarlagadda <67009737+sivasai-yarlagadda@users.noreply.github.com>
2024-07-10 17:29:54 +0530

    Delete notepad_project directory

--- notepad_project/notepad/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.note_list, name='note_list'),
-    path('open/', views.open_file, name='open_file'),
-    path('save/', views.save_file, name='save_file'),
-    path('clear/', views.clear_editor, name='clear_editor'),
-]
